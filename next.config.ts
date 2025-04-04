516ccd1f40d30d79e0be74d57342316f16d4f6f2
amdymstfa <amdymoustapha011@gmail.com>
2025-04-04 14:17:47 +0000

    remove turbopack dependence

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configuration de base uniquement
+  reactStrictMode: true,
+}
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+module.exports = nextConfig
